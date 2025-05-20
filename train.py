1ec9b5b19bdda87a463a319a3585c04840128ada
BakaOvO <ken_zxc@sohu.com>
2025-05-21 00:34:05 +0800

    Add train

--- train.py
+++ train.py
@@ -1,2 +1,9 @@
 from ultralytics import YOLO
 
+model = YOLO(model=r"F:\Project\FPSHelper\yolov12\FPS.yaml",task="detect",verbose=True)
+
+model.train(
+    data=r"F:\Project\FPSHelper\yolov12\FPS.yaml",
+    cfg=r"F:\Project\FPSHelper\yolov12\config.yaml",
+    
+)
